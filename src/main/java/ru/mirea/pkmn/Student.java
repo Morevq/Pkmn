e64f6b52461e313fead9dddc2957503d95472c18
Morevq <zhenulik5@gmail.com>
2024-12-03 14:57:37 +0300

    Use Lombook for less same code

--- src/main/java/ru/mirea/pkmn/Student.java
+++ src/main/java/ru/mirea/pkmn/Student.java
@@ -1,63 +1,20 @@
 package ru.mirea.pkmn;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Student implements Serializable {
     public static final long serialVersionUID = 1L;
-    private String firstName;
-    private String surName;
-    private String patronicName;
-    private String group;
-
-    public Student(String surName, String firstName, String patronicName, String group) {
-        this.firstName = firstName;
-        this.surName = surName;
-        this.patronicName = patronicName;
-        this.group = group;
-    }
-
-    public Student() {
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "firstName='" + firstName + '\'' +
-                ", surName='" + surName + '\'' +
-                ", familyName='" + patronicName + '\'' +
-                ", group='" + group + '\'' +
-                '}';
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getSurName() {
-        return surName;
-    }
-
-    public void setSurName(String surName) {
-        this.surName = surName;
-    }
-
-    public String getPatronicName() {
-        return patronicName;
-    }
-
-    public void setPatronicName(String patronicName) {
-        this.patronicName = patronicName;
-    }
-
-    public String getGroup() {
-        return group;
-    }
-
-    public void setGroup(String group) {
-        this.group = group;
-    }
+    String firstName;
+    String surName;
+    String patronicName;
+    String group;
 }
